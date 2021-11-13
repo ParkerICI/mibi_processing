a96406b68e136d26bd86f0166b75a47a71896f26
Mike Schachter <mschachter@parkerici.org>
2021-11-12 17:08:00 -0800

    added hyperparam search script

---
+++ mibi/nets/bgsub/hyperparam_search.py
@@ -0,0 +1,46 @@
+import os
+
+
+def generate_hyperparam_file(loss_win_sizes=[10, 25],
+                             learning_rates=[1e-3, 1e-2, 1e-1],
+                             weight_decays=[0., 1e-3, 1],
+                             epochs=[5],
+                             num_runs=5,
+                             base_dir='/home/mschachter/CHOP_TMA_pilot',
+                             base_cmd='python mibi/nets/bgsub/train.py'):
+
+    all_params = list()
+
+    chan_file = os.path.join(base_dir, 'info', 'channels.csv')
+    img_dir = os.path.join(base_dir, 'extracted')
+
+    for loss_win_size in loss_win_sizes:
+        for learning_rate in learning_rates:
+            for weight_decay in weight_decays:
+                for epoch in epochs:
+                    all_params.append({'loss_win_size':f"{loss_win_size:.6f}",
+                                       'weight_decay':f"{weight_decay:.6f}",
+                                       'learning_rate':f"{learning_rate:.6f}",
+                                       'epochs':f"{epoch}",
+                                       'images_dir':img_dir,
+                                       'channel_file':chan_file
+                                      })
+
+    cmds = list()
+    for k,params in enumerate(all_params):
+        for j in range(num_runs):
+            model_desc = f'hyperparam_search_{k}_{j}'
+            cmd = base_cmd + ' '.join([f'--{p} {v}' for p,v in params.items()]) + f'--model_desc {model_desc}'
+            cmds.append(cmd)
+
+    print('# of total jobs: ', len(cmds))
+
+    with open('hyperparam_search.sh', 'w') as f:
+        f.write('\n'.join(cmds))
+
+if __name__ == '__main__':
+    generate_hyperparam_file()
+
+
+
+
