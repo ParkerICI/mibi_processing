a9cc05826077c581bd921f5a9e58c5a88608f6a0
Mike Schachter <mschachter@parkerici.org>
2021-12-09 20:24:41 -0800

    added grid plotting function, added some new channel descriptions

--- mibi/preprocess/mibi_plot.py
+++ mibi/preprocess/mibi_plot.py
@@ -35,12 +35,11 @@ def plot_img(img : np.ndarray, title='', transform=True, saturate_percentile=95)
     else:
         img_trans = img
     
+    vmax = img_trans.max()
     if saturate_percentile < 100:
         nz = img_trans > 0
         if nz.sum() > 0:
             vmax = np.percentile(img_trans[nz].ravel(), saturate_percentile)
-    else:
-        vmax = img_trans.max()
     
     plt.imshow(img_trans, interpolation='nearest', aspect='auto', cmap=plt.cm.afmhot, vmin=0, vmax=vmax)
     plt.xlabel('')
@@ -112,3 +111,139 @@ def plot_mibi_image(mp_img : MIBIMultiplexImage, exclude_ignored_channels=True,
     if output_file is not None:
         fig.savefig(output_file)
     return fig
+
+
+def default_plot_func(mp_img, transform, chan_label):
+    gold_index = mp_img.label_to_index[mp_img.gold_channel_label]
+    chan_idx = mp_img.label_to_index[chan_label]
+    if chan_idx == gold_index:
+        img = mp_img.X['raw'][chan_idx]
+    else:
+        img = mp_img.X[transform][chan_idx]
+
+    plot_img(img, transform=False, saturate_percentile=99)
+
+
+def transform_img(img, norm_percentile=99, thresh=0):
+    cimg = img.copy()
+    cimg[cimg < thresh] = 0.
+    timg = np.arcsinh(cimg)
+    p_norm = np.percentile(timg.ravel(), norm_percentile)
+    if np.isnan(p_norm):
+        p_norm = timg.max()
+    timg /= p_norm
+    timg[timg > 1] = 1
+    return timg
+
+
+def check_image(img, transform, chan_label):
+    nz = img > 0
+    if nz.sum() == 0:
+        print(f"image is zero, transform={transform}, chan_label={chan_label}")
+    na = np.isnan(img)
+    if na.sum() > 0:
+        print(f"image has nans, transform={transform}, chan_label={chan_label}")
+
+
+def make_nuclear_merged_image(mp_img, transform, chan_label):
+    
+    nuclear_idx = mp_img.label_to_index['nuclear']
+    chan_idx = mp_img.label_to_index[chan_label]
+
+    nuclear_img = mp_img.X[transform][nuclear_idx].copy()
+    chan_img = mp_img.X[transform][chan_idx].copy()
+
+    img_shape = nuclear_img.shape
+    final_img = np.zeros([img_shape[0], img_shape[1], 4])
+
+    check_image(nuclear_img, transform, 'nuclear')
+    nuclear_img = transform_img(nuclear_img, norm_percentile=100)
+    nuclear_img *= 0.25
+
+    check_image(chan_img, transform, chan_label)
+    chan_img = transform_img(chan_img, norm_percentile=100, thresh=0.0)
+
+    final_img[:, :, 0] = chan_img
+    final_img[:, :, 1] = nuclear_img
+    final_img[:, :, 2] = chan_img
+    final_img[:, :, 3] = 0.95
+
+    return final_img
+
+
+def plot_merged_channels(mp_img, transform, chan_label):
+    img = make_nuclear_merged_image(mp_img, transform, chan_label)
+    plt.imshow(img, interpolation='nearest', aspect='auto')
+    ax = plt.gca()
+    ax.set_xticks([], [])
+    ax.set_yticks([], [])
+
+
+def plot_image_grid(mp_images, channels=None, transform='bgsub',
+                   plot_func=default_plot_func, fov_order=None, fov_labels=dict(),
+                   out_file=None, transpose=False):
+    """ Plots a grid where each column is a FOV and each row is a channel. """
+    
+    if fov_order is None:
+        fov_order = mp_images.keys()
+     
+    plot_height = 8
+    plot_width = 6.5
+
+    ncols = len(fov_order)
+    if channels is None:
+        mp_img = mp_images[fov_order[0]]
+        nrows = len(mp_img.df_channel)
+    else:
+        nrows = len(channels)
+
+    if transpose:
+        ncols_copy = ncols
+        ncols = nrows
+        nrows = ncols_copy
+
+    fig_width = plot_height*ncols
+    fig_height = plot_width*nrows
+
+    fig = plt.figure(figsize=(fig_width, fig_height))
+    gs = plt.GridSpec(nrows, ncols)
+
+    for col_idx,fov in enumerate(fov_order):
+        mp_img = mp_images[fov]
+        gold_index = mp_img.label_to_index[mp_img.gold_channel_label]
+        
+        if channels is None:
+            channel_indices = [gold_index] + list(mp_img.included_channel_indices())
+        else:
+            channel_indices = [mp_img.label_to_index[c] for c in channels]
+
+        if fov in fov_labels:
+            col_title = fov_labels[fov]
+        else:
+            col_title = fov
+
+        for row_idx,chan_idx in enumerate(channel_indices):
+            chan_label = mp_img.df_channel.loc[chan_idx]['Label']
+
+            if transpose:
+                ax = fig.add_subplot(gs[col_idx, row_idx])
+            else:
+                ax = fig.add_subplot(gs[row_idx, col_idx])
+                
+            plot_func(mp_img, transform, chan_label)
+
+            ax.yaxis.set_label_position("right")
+            ax.yaxis.tick_right()
+            plt.ylabel(chan_label, fontsize=25)
+
+            if transpose:    
+                plt.title(col_title, fontsize=25, loc='left')
+            else:
+                if row_idx == 0:
+                    plt.title(col_title, fontsize=20)
+                if row_idx == nrows-1:
+                    plt.title(col_title, y=-0.15, fontsize=20)
+
+    plt.tight_layout()
+    if out_file is not None:
+        fig.savefig(out_file)
